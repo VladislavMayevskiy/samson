9cebe5f0278f6047412f1df65367ea4a64dc0010
Mac <mac@MacBook-Pro-Mac.local>
2025-09-18 15:45:13 +0300

    Create CRUD and connect mongo

---
+++ backend/controllers/ProductController.js
@@ -0,0 +1,66 @@
+import Product from "../models/product.model.js";
+
+export const CreateProduct = async (req, res) => {
+  try {
+    const imagePath = req.file ? `/uploads/${req.file.filename}` : req.body.image || null;
+
+    const product = await Product.create({
+      name: req.body.name,
+      price: req.body.price,
+      quantity: req.body.quantity,
+      description: req.body.description,
+      rating: req.body.rating,
+      image: imagePath
+    });
+
+    res.status(200).json({ message: "Product added", product });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+
+export const GetAll = async(req,res) => {
+    const ProductList = await Product.find()
+    res.json(ProductList)
+};
+
+export const ProductById = async(req,res) => {
+    try{ const ProductId = await Product.findById(req.params.id)
+     if (!ProductId) {
+         return res.status(404).json("ID not found")
+     }
+     res.status(200).json(ProductId)}
+     catch(err) {
+         res.status(500).json({message:err.message})
+     }
+ };
+
+export const ProductDelete =  async (req, res) => {
+    try {
+      const product = await Product.findByIdAndDelete(req.params.id);
+      if (!product) {
+        return res.status(404).json({ message: "Product not found" });
+      }
+      res.status(200).json({ message: "Product deleted successfully" });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  };
+
+export const ProductUpdate =  async (req, res) => {
+    try {
+      const product = await Product.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true } 
+      );
+      if (!product) {
+        return res.status(404).json({ message: "Product not found" });
+      }
+      res.status(200).json({ message: "Product updated successfully", product });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  }
+ 
---
+++ frontend/src/array/CardGoods.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import {
+  Box,
+  Divider,
+  Heading,
+  Text,
+  Button,
+  Image,
+  Badge
+} from "@chakra-ui/react";
+import { FaShoppingCart } from "react-icons/fa";
+
+export default function CardGoods({ product, onAddToCart }) {
+  const {  name, price, quantity ,image,rating} = product;
+
+  return (
+    <Box
+      bgColor="white"
+      borderWidth={2}
+      borderColor="black"
+      borderRadius={15}
+      boxShadow="lg"
+      overflow="hidden"
+      transition="0.3s"
+      _hover={{ transform: "scale(1.03)", boxShadow: "2xl" }}
+      maxW={420}
+      height={500}
+    >
+      {image && (
+        <Image
+        src={`http://localhost:5000${image}`}
+          alt={name}
+          h={{ base: 200, md: 250 }}
+          w="full"
+          objectFit="cover"
+        />
+      )}
+
+      <Divider borderWidth={1} borderColor="black" />
+
+      <Box p={4}>
+        <Heading fontSize={30} mb={1} textColor="black">
+          {name}
+        </Heading>
+
+        <Text mt={3} fontWeight={999} fontSize={25} textColor="green.500">
+          {price} грн
+        </Text>
+
+        <Text fontSize="sm" color="gray.600" mt={1}>
+          В наявності: {quantity}
+        </Text>
+      <Badge bgColor="black" textColor="white">{rating}</Badge>
+        <Button
+          mt={55}
+          w="full"
+          bgColor="green.500"
+          color="white"
+          _hover={{ bgColor: "green.600", color: "white" }}
+          onClick={() => onAddToCart?.(product)}
+          fontWeight={850}
+          leftIcon={<FaShoppingCart />}
+        >
+          Додати в кошик
+        </Button>
+      </Box>
+    </Box>
+  );
+}
